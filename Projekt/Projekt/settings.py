555ce3fb2afbed9fac0488217db4bfc68c7fc392
anngoci <151025177+anngoci@users.noreply.github.com>
2025-04-06 11:16:17 +0200

    Update settings.py

--- Projekt/Projekt/settings.py
+++ Projekt/Projekt/settings.py
@@ -143,6 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'anna.gociekova@gmail.com'
-EMAIL_HOST_PASSWORD = 'kgcv qths ulhi bekl'  # ne normální heslo
+
 DEFAULT_FROM_EMAIL = 'noreply@sportoviste.cz'
